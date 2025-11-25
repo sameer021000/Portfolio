be589b986a5c38cc3dc929f744ced19345c5f843
Sameer Shaik <sameer021000@gmail.com>
2025-11-25 16:54:42 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainScreen from './Folders/MainScreen';
+import About from './Folders/About';
+import Skills from './Folders/Skills';
+import Projects from './Folders/Projects';
+import Experience from './Folders/Experience';
+import Contact from './Folders/Contact';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainScreen />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
